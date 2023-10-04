71c76d9da85fbdb4412d3b362c4740b97afa0266
SalmanTCM <salmann.hossain@gmail.com>
2023-10-04 22:45:42 +0600

    ..

--- next.config.js
+++ next.config.js
@@ -2,16 +2,3 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-
-module.exports = {
-    // ...other configurations...
-  
-    // Add this configuration for static HTML export
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' },
-        // Add other routes if needed
-      };
-    },
-  };
-  
