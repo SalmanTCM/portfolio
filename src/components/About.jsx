6c21649002ea6dde072900983ab5fe3d197f6b0e
SalmanTCM <salmann.hossain@gmail.com>
2023-10-07 02:17:52 +0600

    skill

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,43 +1,38 @@
 import React, { useEffect } from "react";
 import userAvatar from "../../public/avt.png"; // Import your user avatar image
-import Image from 'next/image'
+import Image from 'next/image';
 
 const About = () => {
     useEffect(() => {
-        const text = "Greetings, I'm Md. Salman Hossain, proud to call Comilla my hometown in beautiful Bangladesh. My journey into the realm of technology has been an exciting one.Greetings, I'm Md. Salman Hossain, proud to call Comilla my hometown in beautiful Bangladesh. My journey into the realm of technology has been an exciting one.I laid my educational foundation at Comilla Zilla School for my SSC and continued to Dhaka City College for my HSC, where my fascination with technology truly took flight. This led me to pursue a Bachelor's degree in Computer Science Engineering at East West University.Currently, I'm making strides as a Software Engineer at a prestigious company. In this role, I thrive on crafting innovative solutions that push the boundaries of what's possible in the digital world. My dedication to excellence, along with my penchant for creative problem-solving, has made me an indispensable part of my team.Beyond my professional endeavors, I'm known for my warm personality and my commitment to giving back to my community. I firmly believe that technology can be a force for positive change, and I'm determined to contribute to a brighter, more connected future.In essence, I'm Md. Salman Hossain, and my journey is characterized by innovation, resilience, and unwavering dedication. As I continue to push the boundaries of technology, I'm not just shaping my future but also the future of the tech industry itself. Let's connect and explore the exciting possibilities together!                                     ";
+        const text = "Greetings, I'm Md. Salman Hossain, proud to call Comilla my hometown in beautiful Bangladesh. My journey into the realm of technology has been an exciting one. Greetings, I'm Md. Salman Hossain, proud to call Comilla my hometown in beautiful Bangladesh. My journey into the realm of technology has been an exciting one. I laid my educational foundation at Comilla Zilla School for my SSC and continued to Dhaka City College for my HSC, where my fascination with technology truly took flight. This led me to pursue a Bachelor's degree in Computer Science Engineering at East West University. Currently, I'm making strides as a Software Engineer at a prestigious company. In this role, I thrive on crafting innovative solutions that push the boundaries of what's possible in the digital world. My dedication to excellence, along with my penchant for creative problem-solving, has made me an indispensable part of my team. Beyond my professional endeavors, I'm known for my warm personality and my commitment to giving back to my community. I firmly believe that technology can be a force for positive change, and I'm determined to contribute to a brighter, more connected future. In essence, I'm Md. Salman Hossain, and my journey is characterized by innovation, resilience, and unwavering dedication. As I continue to push the boundaries of technology, I'm not just shaping my future but also the future of the tech industry itself. Let's connect and explore the exciting possibilities together!                                     ";
         const typingTextElement = document.getElementById("typing-text");
         let charIndex = 0;
 
-        function type() {
-            if (charIndex < text.length) {
-                typingTextElement.innerHTML += text.charAt(charIndex);
-                charIndex++;
-                setTimeout(type, 50); // Typing speed (milliseconds)
-            }
-        }
+        // function type() {
+        //     if (charIndex < text.length) {
+        //         typingTextElement.innerHTML += text.charAt(charIndex);
+        //         charIndex++;
+        //         setTimeout(type, 50); // Typing speed (milliseconds)
+        //     }
+        // }
 
-        type();
+        // type();
     }, []);
 
     return (
-        <div className="about-section">
-            <div className="parallax-background">
-                
-                {/* <div className="triangle"></div>
-                <div className="circle"></div>
-                <div className="color-scope"></div> */}
-            </div>
-            <div className="content-container border-4 border-blue-500 border-white custom-border shadow-2xl">
+        <div className="about-section  min-h-screen flex flex-col justify-center items-center">
+            <div className="content-container border-4 border-blue-500 border-white custom-border shadow-2xl p-8 rounded-lg">
                 <Image
                     src={userAvatar}
-                    width={500}
-                    height={500}
-                    className="user-avatar hover:translate-x-2 hover:translate-y-2"
+                    width={200}
+                    height={200}
+                    className="user-avatar hover:transform hover:scale-105"
                     alt="User Avatar"
                 />
-                <div className="text-container">
-                    <h2>About Me</h2>
-                    <p id="typing-text"></p>
+                <div className="text-container mt-8">
+                    <h2 className="text-3xl font-semibold mb-4">About Me</h2>
+                    <p id="typing-text" className="text-white">Greetings, I'm Md. Salman Hossain, proud to call Comilla my hometown in beautiful Bangladesh. My journey into the realm of technology has been an exciting one. Greetings, I'm Md. Salman Hossain, proud to call Comilla my hometown in beautiful Bangladesh. My journey into the realm of technology has been an exciting one. I laid my educational foundation at Comilla Zilla School for my SSC and continued to Dhaka City College for my HSC, where my fascination with technology truly took flight. This led me to pursue a Bachelor's degree in Computer Science Engineering at East West University. Currently, I'm making strides as a Software Engineer at a prestigious company. In this role, I thrive on crafting innovative solutions that push the boundaries of what's possible in the digital world. My dedication to excellence, along with my penchant for creative problem-solving, has made me an indispensable part of my team. Beyond my professional endeavors, I'm known for my warm personality and my commitment to giving back to my community. I firmly believe that technology can be a force for positive change, and I'm determined to contribute to a brighter, more connected future. In essence, I'm Md. Salman Hossain, and my journey is characterized by innovation, resilience, and unwavering dedication. As I continue to push the boundaries of technology, I'm not just shaping my future but also the future of the tech industry itself. Let's connect and explore the exciting possibilities together!
+</p>
                 </div>
             </div>
         </div>
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,38 +1,104 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { FaLinkedin } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
 
 const Skill = () => {
+  const skills = [
+    { name: "React", icon: <FaLinkedin className="text-4xl text-blue-500" /> },
+    { name: "Next.js", icon: <FaLinkedin className="text-4xl text-gray-500" /> },
+    { name: "Bootstrap", icon: <FaLinkedin className="text-4xl text-purple-500" /> },
+    { name: "Tailwind CSS", icon: <FaLinkedin className="text-4xl text-cyan-500" /> },
+    { name: "Python", icon: <FaLinkedin className="text-4xl text-yellow-500" /> },
+    { name: "Django", icon: <FaLinkedin className="text-4xl text-green-500" /> },
+    { name: "PHP", icon: <FaLinkedin className="text-4xl text-indigo-500" /> },
+    { name: "Laravel", icon: <FaLinkedin className="text-4xl text-red-500" /> },
+    { name: "Docker", icon: <FaLinkedin className="text-4xl text-blue-600" /> },
+    { name: "Selenium", icon: <FaLinkedin className="text-4xl text-green-600" /> },
+    { name: "UI Path", icon: <FaLinkedin className="text-4xl text-yellow-600" /> },
+    { name: "MySQL", icon: <FaLinkedin className="text-4xl text-blue-500" /> },
+    { name: "GraphQL", icon: <FaLinkedin className="text-4xl text-pink-500" /> },
+  ];
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1, // Adjust the threshold as needed
+  });
+
   return (
-    <div className=" flex flex-warp bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition duration-300 ease-in-out cursor-pointer transform hover:-translate-y-2">
-      {skills.map((skill, index) => (
-        <div
-          key={index}
-          className=""
-        >
-          <div className="bg-white p-2 rounded-lg shadow-md hover:shadow-xl transition duration-300 ease-in-out cursor-pointer transform hover:-translate-y-2">
-            {skill.icon}
-          </div>
-          <span className="mt-2">{skill.name}</span>
-        </div>
-      ))}
+    <div className="container mx-auto my-10">
+      <div className="grid grid-cols-7 gap-6">
+        {skills.map((skill, index) => (
+          <motion.div
+            key={index}
+            className="bg-[#0A192F] p-4 rounded-lg shadow-md transition duration-300 ease-in-out cursor-pointer transform m-2"
+            initial={{
+              x: index % 7 === 0 ? -100 : 100, // Start a new row
+              opacity: 0,
+            }}
+            animate={{
+              x: inView ? 0 : index % 7 === 0 ? -100 : 100, // Start a new row
+              opacity: inView ? 1 : 0,
+            }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5 }}
+            ref={ref}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+          >
+            <div className="text-center">
+              <span className="mt-2 text-white">{skill.icon}</span>
+              <span className="mt-2 text-white">{skill.name}</span>
+            </div>
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 };
 
-const skills = [
-  { name: "React", icon: <FaLinkedin className="text-4xl text-blue-500" /> },
-  { name: "Next.js", icon: <FaLinkedin className="text-4xl text-gray-500" /> },
-  { name: "Bootstrap", icon: <FaLinkedin className="text-4xl text-purple-500" /> },
-  { name: "Tailwind CSS", icon: <FaLinkedin className="text-4xl text-cyan-500" /> },
-  { name: "Python", icon: <FaLinkedin className="text-4xl text-yellow-500" /> },
-  { name: "Django", icon: <FaLinkedin className="text-4xl text-green-500" /> },
-  { name: "PHP", icon: <FaLinkedin className="text-4xl text-indigo-500" /> },
-  { name: "Laravel", icon: <FaLinkedin className="text-4xl text-red-500" /> },
-  { name: "Docker", icon: <FaLinkedin className="text-4xl text-blue-600" /> },
-  { name: "Selenium", icon: <FaLinkedin className="text-4xl text-green-600" /> },
-  { name: "UI Path", icon: <FaLinkedin className="text-4xl text-yellow-600" /> },
-  { name: "MySQL", icon: <FaLinkedin className="text-4xl text-blue-500" /> },
-  { name: "GraphQL", icon: <FaLinkedin className="text-4xl text-pink-500" /> },
-];
-
 export default Skill;
+
+
+
+
+// import React from "react";
+// import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+
+// const Skill = () => {
+//   const skills = [
+    // { name: "React", icon: <FaLinkedin className="text-4xl text-blue-500" /> },
+    // { name: "Next.js", icon: <FaLinkedin className="text-4xl text-gray-500" /> },
+    // { name: "Bootstrap", icon: <FaLinkedin className="text-4xl text-purple-500" /> },
+    // { name: "Tailwind CSS", icon: <FaLinkedin className="text-4xl text-cyan-500" /> },
+    // { name: "Python", icon: <FaLinkedin className="text-4xl text-yellow-500" /> },
+    // { name: "Django", icon: <FaLinkedin className="text-4xl text-green-500" /> },
+    // { name: "PHP", icon: <FaLinkedin className="text-4xl text-indigo-500" /> },
+    // { name: "Laravel", icon: <FaLinkedin className="text-4xl text-red-500" /> },
+    // { name: "Docker", icon: <FaLinkedin className="text-4xl text-blue-600" /> },
+    // { name: "Selenium", icon: <FaLinkedin className="text-4xl text-green-600" /> },
+    // { name: "UI Path", icon: <FaLinkedin className="text-4xl text-yellow-600" /> },
+    // { name: "MySQL", icon: <FaLinkedin className="text-4xl text-blue-500" /> },
+    // { name: "GraphQL", icon: <FaLinkedin className="text-4xl text-pink-500" /> },
+//   ];
+
+//   return (
+//     <div className="container mx-auto my-10">
+//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+//         {skills.map((skill, index) => (
+//           <div
+//             key={index}
+//             className="bg-[#0A192F] p-4 rounded-lg shadow-md hover:shadow-xl transition duration-300 ease-in-out cursor-pointer transform hover:-translate-y-2"
+//           >
+//             <div className="text-center">
+//               {skill.icon}
+//               <span className="mt-2 text-white">{skill.name}</span>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+//  export default Skill;
